0875f9c5fdf095c0214bf1024d60f7fe3c554f3c
XimeCruz <xmenacruzt@gmail.com>
2024-03-06 22:24:09 -0400

    llenando atributos de Usuario.java

--- src/main/java/com/example/donacion/model/Usuario.java
+++ src/main/java/com/example/donacion/model/Usuario.java
@@ -1,2 +1,84 @@
-package com.example.donacion.model;public class Usuario {
+package com.example.donacion.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long idUser ;
+
+    private String nombre;
+
+    private String contrasenia;
+
+    private String ubicacion;
+
+    private String correo;
+
+    private String telefono;
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUser=" + idUser +
+                ", nombre='" + nombre + '\'' +
+                ", contrasenia='" + contrasenia + '\'' +
+                ", ubicacion='" + ubicacion + '\'' +
+                ", correo='" + correo + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+
+
 }
